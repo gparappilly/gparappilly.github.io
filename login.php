73d9fa302c5668a0bc98362c7345f094c8583cb4
Geordie Parappilly <geordie.p@hotmail.com>
2018-05-01 18:45:59 -0700

    test

---
+++ login.php
@@ -0,0 +1,36 @@
+
+<html>
+
+<head>
+    <title>Login</title>
+
+</head>
+
+<body>
+        
+    
+    <form method="post" action="lesson12_ex1a.php">
+
+        <p>Username:
+            <input type="text" name="username" />
+        </p>
+        <p>Password:
+            <input type="text" name="password" />
+        </p>
+
+        <p>
+            <input type="submit" value="Let me in" />
+        </p>
+
+        <p>Username:
+            <strong>php</strong>
+            <br />Password:
+            <b>php</b>
+            <br>
+            <p>Also, try to log on with a different password or username - then you will not be able to see the protected file.</p>
+
+
+    </form>
+</body>
+
+</html>
